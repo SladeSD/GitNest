a52b8c3c4b63cb75a999031b07796720caa7f0b7
SladeSD <dodoslade@gmail.com>
2024-06-02 18:43:20 +0200

    GitNest Monster

---
+++ folderWatcher.js
@@ -0,0 +1,43 @@
+
+const { Octokit } = require("@octokit/rest")
+const chokidar = require('chokidar')
+const simpleGit = require('simple-git')
+const fs = require('fs')
+
+const octokit = new Octokit({ auth: '' }) // github token
+
+const projektiDir = "E:\\projekti"
+
+function isValidName(name) {
+  return /^[a-z0-9_-]+$/.test(name) && !/^[0-9_-]/.test(name) // regex magija
+}
+
+const watcher = chokidar.watch(projektiDir, { ignored: /^\./, persistent: true, depth: 0, ignoreInitial: true })
+
+watcher.on('addDir', async (path) => {
+  const folderName = path.split('\\').pop()
+
+  if (!isValidName(folderName)) {
+    console.log(`Invalid folder name: ${folderName}, skipping...`)
+    return;
+  }
+
+  try {
+    const response = await octokit.repos.createForAuthenticatedUser({
+      name: folderName,
+      auto_init: true // ovo je za README
+    });
+    console.log(`GitHub repository created: ${response.data.html_url}`)
+
+    const git = simpleGit(path)
+    await git.init()
+    await git.addRemote('origin', response.data.clone_url)
+    
+    console.log(`Git repository initialized and remote set for ${folderName}`)
+  } 
+  catch (error) {
+    console.error('Failed to create repository or initialize Git:', error)
+  }
+});
+
+console.log(`Watching for new folders in ${projektiDir}`)
